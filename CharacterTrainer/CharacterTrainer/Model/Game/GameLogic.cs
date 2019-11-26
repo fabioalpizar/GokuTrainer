af908bb1e124dabd770aa844fff54c332cc556d0
gaboq98 <tortuloca@gmail.com>
2019-11-26 00:29:29 -0300

    game logic

--- CharacterTrainer/CharacterTrainer/Model/Game/GameLogic.cs
+++ CharacterTrainer/CharacterTrainer/Model/Game/GameLogic.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CharacterTrainer.Model.Activities;
+using CharacterTrainer.Model.Rooms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CharacterTrainer.Model
@@ -9,8 +12,77 @@ namespace CharacterTrainer.Model
     class GameLogic
     {
 
-        private Character Character;
-        private GameTime Time;
+        private Character CurrentCharacter { get; set; }
+        private GameTime Time { get; set; }
+        private bool Running { get; set; }
+        private IStrategy CurrentActivity { get; set; }
+        private int AttackTime1 { get; set; }
+        private int AttackTime2 { get; set; }
+        private int SocializeTime { get; set; }
+
+        public GameLogic(int dayDuration, int yearDuration)
+        {
+            Time = new GameTime(dayDuration, yearDuration);
+            //CurrentCharacter = CharacterFactory.newCharacter();
+            CurrentActivity = new Walk();
+            Thread thr = new Thread(new ThreadStart(GameCicle));
+            thr.Start();
+        }
+
+        public void GameCicle()
+        {
+            while(Running)
+            {
+                if (Time.IsNewDay())
+                {
+                    if (Time.IsNewYear())
+                    {
+                        NewYear();
+                    }
+                    NewDay();
+                }
+                CheckRandomEvents();
+
+                Thread.Sleep(1000);
+                Time.incrementTime();
+            }
+        }
+
+        public void CheckRandomEvents()
+        {
+            if (Time.GetCurrentTime() == AttackTime1)
+            {
+                CurrentActivity = new Fight();
+            }
+            if (Time.GetCurrentTime() == AttackTime2)
+            {
+                CurrentActivity = new Fight();
+            }
+            if (Time.GetCurrentTime() == SocializeTime)
+            {
+                CurrentActivity = new Friends();
+            }
+        }
+
+        public void CheckStats()
+        {
+            //CurrentCharacter.CheckStats();
+        }
+
+        public void NewDay()
+        {
+            Random rnd = new Random();
+            AttackTime1 = rnd.Next(Time.GetTime());
+            AttackTime2 = rnd.Next(Time.GetTime());
+            SocializeTime = rnd.Next(Time.GetTime());
+            CheckStats();
+        }
+
+        public void NewYear()
+        {
+            //CurrentCharacter.LevelUp();
+        }
 
     }
+
 }
